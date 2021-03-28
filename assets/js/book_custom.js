dcce089e1789bb8efa562cef27a091381d37efef
coolaquas <samrat.mallick1@gmail.com>
2021-03-28 08:12:29 +0530

    done

---
+++ assets/js/book_custom.js
@@ -0,0 +1,292 @@
+let Bookdata = [
+  {
+    title: "Principles of Managment",
+    info1: "Notes : DR. TINY TANUSHREE GOHAIN",
+    info2: "MANAGEMENT I [HSMC (CSE) 401]",
+    src:
+      "https://drive.google.com/file/d/1KUMNqUvKiq8ekbuo0SXcoOAtbt1K-WVH/view",
+    image: "11.png",
+  },
+  {
+    title: "Organising",
+    info1: "Notes : DR. TINY TANUSHREE GOHAIN",
+    info2: "MANAGEMENT I [HSMC (CSE) 401]",
+    src:
+      "https://drive.google.com/file/d/13s_Ms0wvdB9SOPtHiC_J_hcKxN-iVgXr/view",
+    image: "10.png",
+  },
+  {
+    title: "Design & analysis of algorithm Notes",
+    info1: "Notes : SUPRATIV SAHA",
+    info2: "Design & analysis of algorithm",
+    src:
+      "https://www.brainwareuniversity.ac.in/studentselfservice/student-student-forum-new.php?type=topic&actiontype=modify&boardid=95&topicid=10664",
+    image: "9.png",
+  },
+  {
+    title: "Fundamental of Computer",
+    info1: "Notes : SITIKANTHA CHATTOPADHYAY",
+    info2: "Computer orgnaization and architecture",
+    src:
+      "https://drive.google.com/file/d/1b804CPjPTWcM53517fToSE8zwKkFSXkv/view",
+    image: "8.png",
+  },
+  {
+    title: "Manegment",
+    info1: "Notes : Anupam Bandyopadhyay",
+    info2: "MANAGEMENT I [HSMC (CSE) 401]",
+    src:
+      "https://drive.google.com/file/d/10GHvyXnJcFezpjLYNIMFuMOVxaINcm1Z/view",
+    image: "7.png",
+  },
+  {
+    title: "Unit 1: Introduction to Management",
+    info1: "Notes : Anupam Bandyopadhyay",
+    info2: "MANAGEMENT I [HSMC (CSE) 401]",
+    src:
+      "https://drive.google.com/file/d/1b804CPjPTWcM53517fToSE8zwKkFSXkv/view",
+    image: "6.png",
+  },
+  {
+    title: "Basic Structure of Computer",
+    info1: "Notes : SITIKANTHA CHATTOPADHYAY",
+    info2: "Computer orgnaization and architecture",
+    src:
+      "https://drive.google.com/file/d/1TJJLropb-uLCnwhzwbsVeu-lboPbfb-1/view",
+    image: "5.png",
+  },
+  {
+    title: "Notes 1: Operating System",
+    info1: "Notes : PRIYANKA GHOSH",
+    info2: "Operating System",
+    src:
+      "https://drive.google.com/file/d/12Uv-45739C4VsbxGOxvqmuaMkru-tA7p/view",
+    image: "4.png",
+  },
+  {
+    title: "Computer Organization and Architecture - Module-1",
+    info1: "Notes : SITIKANTHA CHATTOPADHYAY",
+    info2: "Computer orgnaization and architecture",
+    src:
+      "https://drive.google.com/file/d/10GHvyXnJcFezpjLYNIMFuMOVxaINcm1Z/view",
+    image: "3.png",
+  },
+  {
+    title: "Theory and Problems of DISCRETE MATHEMATICS Third Edition",
+    info1: "SEYMOUR LIPSCHUTZ, Ph.D.</br>Temple University",
+    info2: "MARC LARS LIPSON, Ph.D.</br>University of Virginia",
+    src:
+      "https://drive.google.com/file/d/1o_V4iIYFmz-xYcXPOKd0sWH6h4LuXA1M/view",
+    image: "2.png",
+  },
+  {
+    title: "Discrete Mathematics and Its Applications",
+    info1: "Kenneth H. Rosen</br>Monmouth University",
+    info2: "(and formerly AT&T Laboratories)",
+    src:
+      "https://drive.google.com/file/d/10GHvyXnJcFezpjLYNIMFuMOVxaINcm1Z/view",
+    image: "1.png",
+  },
+];
+
+Bookdata.forEach((info, idx) => {
+  if (idx % 2 === 0) {
+    $("#book_data").append(`
+    <section class="section padding-top-70 padding-bottom-0"id="${
+      idx === 0 ? "Study_Material" : ""
+    }">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-5 col-md-12 col-sm-12 align-self-center"
+                    data-scroll-reveal="enter left move 30px over 0.6s after 0.4s">
+                    <a href="${info?.src}" target="_blank">
+                        <img src="/assets/images/${
+                          info?.image
+                        }" class="rounded img-fluid d-block mx-auto" alt="App"
+                            style="height: 350px" />
+                    </a>
+                </div>
+                <div class="col-lg-1"></div>
+                <div class="col-lg-6 col-md-12 col-sm-12 align-self-center mobile-top-fix">
+                    <div class="left-heading">
+                        <h2 class="section-title">
+                            ${info?.title}</h2>
+                    </div>
+                    <div class="left-text">
+                        <p>
+                            ${info?.info1}
+                            <br />
+                            ${info?.info2}
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="hr"></div>
+                </div>
+            </div>
+        </div>
+    </section>
+    `);
+  } else {
+    $("#book_data").append(`
+    <section class="section padding-bottom-100">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-md-12 col-sm-12 align-self-center mobile-bottom-fix">
+                    <div class="left-heading">
+                        <h2 class="section-title">
+                        ${info?.title}
+                        </h2>
+                    </div>
+                    <div class="left-text">
+                        <p>
+
+
+                        ${info?.info1}
+                        <br />
+                        ${info?.info2}
+                        </p>
+                    </div>
+                </div>
+                <div class="col-lg-1"></div>
+                <div class="col-lg-5 col-md-12 col-sm-12 align-self-center mobile-bottom-fix-big"
+                    data-scroll-reveal="enter right move 30px over 0.6s after 0.4s">
+                    <a href="${info?.src}"
+                        target="_blank">
+                        <img src=/assets/images/${info?.image} class="rounded img-fluid d-block mx-auto" alt="App"
+                            style="height: 350px" />
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    `);
+  }
+});
+
+(function ($) {
+  "use strict";
+
+  // Window Resize Mobile Menu Fix
+  mobileNav();
+
+  // Scroll animation init
+  window.sr = new scrollReveal();
+
+  // Menu Dropdown Toggle
+  if ($(".menu-trigger").length) {
+    $(".menu-trigger").on("click", function () {
+      $(this).toggleClass("active");
+      $(".header-area .nav").slideToggle(200);
+    });
+  }
+
+  // Menu elevator animation
+  $("a[href*=\\#]:not([href=\\#])").on("click", function () {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        var width = $(window).width();
+        if (width < 991) {
+          $(".menu-trigger").removeClass("active");
+          $(".header-area .nav").slideUp(200);
+        }
+        $("html,body").animate(
+          {
+            scrollTop: target.offset().top - 130,
+          },
+          700
+        );
+        return false;
+      }
+    }
+  });
+
+  $(document).ready(function () {
+    //smoothscroll
+    $('a[href^="#"]').on("click", function (e) {
+      e.preventDefault();
+      $(document).off("scroll");
+
+      $("a").each(function () {
+        $(this).removeClass("active");
+      });
+      $(this).addClass("active");
+
+      var target = this.hash,
+        menu = target;
+      var target = $(this.hash);
+      $("html, body")
+        .stop()
+        .animate(
+          {
+            scrollTop: target.offset().top - 130,
+          },
+          500,
+          "swing",
+          function () {
+            window.location.hash = target;
+          }
+        );
+    });
+  });
+
+  // Home seperator
+  if ($(".home-seperator").length) {
+    $(".home-seperator .left-item, .home-seperator .right-item").imgfix();
+  }
+
+  // Home number counterup
+  if ($(".count-item").length) {
+    $(".count-item strong").counterUp({
+      delay: 10,
+      time: 1000,
+    });
+  }
+
+  // Page loading animation
+  $(window).on("load", function () {
+    if ($(".cover").length) {
+      $(".cover").parallax({
+        imageSrc: $(".cover").data("image"),
+        zIndex: "1",
+      });
+    }
+
+    $("#preloader").animate(
+      {
+        opacity: "0",
+      },
+      600,
+      function () {
+        setTimeout(function () {
+          $("#preloader").css("visibility", "hidden").fadeOut();
+        }, 300);
+      }
+    );
+  });
+
+  // Window Resize Mobile Menu Fix
+  $(window).on("resize", function () {
+    mobileNav();
+  });
+
+  // Window Resize Mobile Menu Fix
+  function mobileNav() {
+    var width = $(window).width();
+    $(".submenu").on("click", function () {
+      if (width < 992) {
+        $(".submenu ul").removeClass("active");
+        $(this).find("ul").toggleClass("active");
+      }
+    });
+  }
+})(window.jQuery);
--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,395 @@
+let subject = [
+  {
+    name: "Design & analysis of algorithm",
+    code: "DAA",
+    data: [
+      {
+        type: "Lab",
+        date: "24-03-2021",
+        media: "1x9Nc7i-uvZ5-oh4pYeXB5Au00_aN9_G3",
+      },
+      {
+        type: "Lab",
+        date: "19-03-2021",
+        media: "1ri1YLp5o0rBobeWAUO6EdLdI1Mh9ltJT",
+      },
+      {
+        type: "Theory",
+        date: "13-03-2021",
+        media: "1cY4sCWNITPxzwO8x3nGt_gsUPY_HAiGf",
+      },
+      {
+        type: "Lab",
+        date: "12-03-2021",
+        media: "1biR8nHT4_4VDzdMdspICuMEf4wWEG0pQ",
+      },
+      {
+        type: "Theory",
+        date: "12-03-2021",
+        media: "1azoosWgEg7bBX2WRno1NtIaBNzNQbXav",
+      },
+      {
+        type: "Theory",
+        date: "06-03-2021",
+        media: "1o3pdoOxilXsWT52495uBhkGafAsHtjWd",
+      },
+      {
+        type: "Lab",
+        date: "05-03-2021",
+        media: "1QYzxlUdXhslhGdlg3Sw7waQXHjkQR11M",
+      },
+
+      {
+        type: "Theory",
+        date: "05-03-2021",
+        media: "1S8IaHl8o7GNG263Szu4eN7W6gz43FtP5",
+      },
+      {
+        type: "Theory",
+        date: "27-02-2021",
+        media: "1biR8nHT4_4VDzdMdspICuMEf4wWEG0pQ",
+      },
+      {
+        type: "Lab",
+        date: "26-02-2021",
+        media: "1kIKhOdExx_sKkNoLP6u_-r_b-RvOh9da",
+      },
+      {
+        type: "Theory",
+        date: "26-02-2021",
+        media: "1z4kq8UOBdOp3vqIUK6DbxgTzam0YU5rQ",
+      },
+      {
+        type: "Theory",
+        date: "20-02-2021",
+        media: "1ZOpEa0Tq_mY8q5Esg3VLtSoL_nuZXvDS",
+      },
+      {
+        type: "Theory",
+        date: "19-02-2021",
+        media: "10Tj9CqynJvXV16WDxL76uRhPpESWD4FP",
+      },
+      {
+        type: "Theory",
+        date: "13-02-2021",
+        media: "1y0rgsOw8sihutWs0jq7Ew62wjc4Bw7n7",
+      },
+      {
+        type: "Theory",
+        date: "12-02-2021",
+        media: "1eIhbEc-3FKumwWJSYTj13WuM98DqxE9G",
+      },
+      {
+        type: "Theory",
+        date: "12-02-2021",
+        media: "1iU7VAAZO2Z-ONGoUH92wwFd9Fi-m-fuo",
+      },
+    ],
+  },
+  {
+    name: "Operating System",
+    code: "OS",
+    data: [
+      {
+        type: "Theory",
+        date: "25-03-2021",
+        media: "1fCvcIlwe_J1YWwltz3aCufeLkSYKS5Q5",
+      },
+      {
+        type: "Theory",
+        date: "24-03-2021",
+        media: "1OwPMX5iFInzoDGGLJpTGwV9BpT0VR9pD",
+      },
+      {
+        type: "Lab",
+        date: "23-03-2021",
+        media: "13hfRWEY4mgcihVeUBQaa5tFxHWq0KdsI",
+      },
+      {
+        type: "Theory",
+        date: "18-03-2021",
+        media: "1YqyT9iu-DYsff1aMqR8iSeq1cZt9OXfq",
+      },
+      {
+        type: "Theory",
+        date: "17-03-2021",
+        media: "1yoYvMn6s_K7mgx4dSUcVhoJgwYaeqa-a",
+      },
+      {
+        type: "Lab",
+        date: "16-03-2021",
+        media: "1k_Y4w9zM1277cwOU040sGCttIxZDV91a",
+      },
+      {
+        type: "Theory",
+        date: "11-03-2021",
+        media: "1g8t4sWQqA19XLCBkXWUkWxxq-ckVBAtU",
+      },
+      {
+        type: "Theory",
+        date: "10-03-2021",
+        media: "1ho7QZ-99VMaLFAMOG6KhPTGVVD0oSR4a",
+      },
+      {
+        type: "Lab",
+        date: "09-03-2021",
+        media: "125CIOTBF23Qy5OFB_ViUNnuxb1Nnk7F4",
+      },
+      {
+        type: "Theory",
+        date: "04-03-2021",
+        media: "1c067uRi2n-iwpruj73mYzrtZOWU_-vhx",
+      },
+      {
+        type: "Theory",
+        date: "03-03-2021",
+        media: "1XUwn__i9ZB74xppMqSU99odcNg6p0433",
+      },
+      {
+        type: "Lab",
+        date: "02-03-2021",
+        media: "1PB-sLVxdKoLsl1yqDu-5bTGkcee0W2_M",
+      },
+      {
+        type: "Theory",
+        date: "25-02-2021",
+        media: "1VODDvmsSEArBVoP55IYT1Oozx3iwePg4",
+      },
+      {
+        type: "Theory",
+        date: "18-02--2021",
+        media: "1hA5bdGr9sJ9dJh8Mcdsmxfu2IIaRH3ph",
+      },
+      {
+        type: "Theory",
+        date: "16-02-2021",
+        media: "1Es23kwdTlc__Hikd-iqhyG6p_crnr-GP",
+      },
+      {
+        type: "Theory",
+        date: "11-02-2021",
+        media: "1CJ_bV72qx9wM2bWi6l4c7rH8CPe6z75j",
+      },
+      {
+        type: "Lab",
+        date: "09-02--2021",
+        media: "1DA7OYNISujXYxOrB0m1OQLZu2bY28z3S",
+      },
+    ],
+  },
+  {
+    name: "Computer orgnaization and architecture",
+    code: "COA",
+    data: [
+      {
+        type: "Theory",
+        date: "27-03-2021",
+        media: "1rc7I0TIqIdrwLHOoROpeec4kSxppD55H",
+      },
+      {
+        type: "Theory",
+        date: "25-03-2021",
+        media: "1DNOijish-xtgcVgfqgVOqoOce1iG-CWB",
+      },
+      {
+        type: "Lab",
+        date: "24-03-2021",
+        media: "1cMHN97mDoTn4D2_gmH6685TuaHB69Pdp",
+      },
+      {
+        type: "Theory",
+        date: "20-03-2021",
+        media: "1rV1jEJT3c6CEvyyxKnQfz3dzFsXK9vPO",
+      },
+      {
+        type: "Theory",
+        date: "18-03-2021",
+        media: "1hvk9SnUb1lur5iGufj7PIz3-QVGNK5X8",
+      },
+      {
+        type: "Theory",
+        date: "13-03-2021",
+        media: "1blO_qupqF_onjP8OZjCerycS9E8tQAFx",
+      },
+      {
+        type: "Lab",
+        date: "10-03-2021",
+        media: "1vh1e7By6aTjkFigliYhxBOZwLpdXYByM",
+      },
+      {
+        type: "Theory",
+        date: "06-03-2021",
+        media: "1YYDbznZlSTO3jQ3XzHV_k123O5XylNlM",
+      },
+      {
+        type: "Theory",
+        date: "04-03-2021",
+        media: "1AK2zOFQenijlKHwpruhDukc9-EePSyQl",
+      },
+      {
+        type: "Lab",
+        date: "03-03-2021",
+        media: "1DllZosKA3lOy3d5DguRQfem90VlUOwr4",
+      },
+      {
+        type: "Theory",
+        date: "27-02-2021",
+        media: "1VzmT51YXcw_6i8LxeKPcvpmG_1grTWzx",
+      },
+      {
+        type: "Lab",
+        date: "24-02-2021",
+        media: "1iNBwP18ro3kXceuEZEz-3VwOJuS3XsML",
+      },
+      {
+        type: "Theory",
+        date: "20-02-2021",
+        media: "1rizlif6OoObsyF4sVrxZzzXdOqwe-wTb",
+      },
+      {
+        type: "Theory",
+        date: "18-02-2021",
+        media: "1czW7eHgixxL4_CVJmtKn0mGW8TBp9fDe",
+      },
+      {
+        type: "Lab",
+        date: "17-02-2021",
+        media: "1auIyB2oma2ktY_fV3pzRzMZxKJptOT_w",
+      },
+      {
+        type: "Theory",
+        date: "13-02-2021",
+        media: "1sx8_4wwtH1uEXCyS4TJlf-jtm4IAVEUW",
+      },
+      {
+        type: "Theory",
+        date: "10-02-2021",
+        media: "1NvzIKPnMcU4k7HXHstwEe_vC7gjk-Chn",
+      },
+      {
+        type: "Lab",
+        date: "10-02-2021",
+        media: "1J9ooDOIDEMMvr2U6TGmYmlpwKfPVM11P",
+      },
+    ],
+  },
+  {
+    name: "Discrete Mathematics",
+    code: "DM",
+    data: [
+      {
+        type: "Theory",
+        date: "20-03-2021",
+        media: "1vTvfs8ANoQ6hN3z3Lj6g_2SFN_gsPfBy",
+      },
+      {
+        type: "Theory",
+        date: "19-03-2021",
+        media: "1Rka0FtZxBM8ccYcp7GzVOsAWktBSZ-0d",
+      },
+      {
+        type: "Theory",
+        date: "16-03-2021",
+        media: "16cHokFwlnam3we5Usu3aLuufAG5_smqF",
+      },
+      {
+        type: "Theory",
+        date: "13-03-2021",
+        media: "18VxBk_m9AQSjD_9qNsPhvYnDd1-6Cr7g",
+      },
+      {
+        type: "Theory",
+        date: "12-03-2021",
+        media: "1lLsCAiaB2ky4x_6p38AdLjqJTt1YIok2",
+      },
+      {
+        type: "Theory",
+        date: "09-03-2021",
+        media: "1hZOYRtLXX2WQmsGwaRRvSBG3L-Bb3n-2",
+      },
+      {
+        type: "Theory",
+        date: "06-03-2021",
+        media: "1pPis1_P9vqJ8gqBcWLZ5UxQ7PyL_hCad",
+      },
+      {
+        type: "Theory",
+        date: "05-03-2021",
+        media: "1OeOdtwdjKQYyTFMUfWqSJPSUQ4GtioP7",
+      },
+      {
+        type: "Theory",
+        date: "02-03-2021",
+        media: "1ctshwInJyNAzpKljGv7bK8UTHDIN7-ZQ",
+      },
+      {
+        type: "Theory",
+        date: "26-02-2021",
+        media: "19xXYhaNav33Qguh4jE6ouuXuWmeJO8B",
+      },
+      {
+        type: "Theory",
+        date: "25-02-2021",
+        media: "1lsb9BNBK8NdKgN8KEFy4jzz6xDT04FVK",
+      },
+      {
+        type: "Theory",
+        date: "22-02-2021",
+        media: "1XVH3MXrGi_5_cUlStuTFScd67yjHEpiw",
+      },
+      {
+        type: "Theory",
+        date: "20-02-2021",
+        media: "1CFRvMRsb2jrVSq4EEs1JOa2xipmdyBZT",
+      },
+      {
+        type: "Theory",
+        date: "19-02-2021",
+        media: "1kpwrYmDRulH4szLSOZDZJn0vC2FxFLfu",
+      },
+      {
+        type: "Theory",
+        date: "09-02-2021",
+        media: "1AEbiZRLlsY91ISNCEEKWJhKINQzox_ZR",
+      },
+    ],
+  },
+  {
+    name: "Managment I",
+    code: "MN",
+    data: [
+      {
+        type: "No Video Yet",
+        date: "Oh ho !",
+        media: "NA",
+      },
+    ],
+  },
+  {
+    name: "Linux Lab",
+    code: "LN",
+    data: [
+      {
+        type: "No Video Yet",
+        date: "Oh ho !",
+        media: "NA",
+      },
+    ],
+  },
+  {
+    name: "English II",
+    code: "ENG",
+    data: [
+      {
+        type: "No Video Yet",
+        date: "Oh ho !",
+        media: "NA",
+      },
+    ],
+  },
+];
+
+
 (function ($) {
   "use strict";
 
@@ -183,3 +575,37 @@ $("#myModalPrev").on("show.bs.modal", function (e) {
 $("#myModalPrev").on("hidden.bs.modal", function () {
   $("#myModalPrev .modal-body").empty();
 });
+
+let add_Data = (subCode, data) => {
+  // console.log("data", data);
+  data.forEach((item, ind) => {
+    $(`#${subCode}`).append(`
+    <a class="list-group-item list-group-item-action list-group-item-success"
+    data-id="${item.date} - ${item.type}~${item.media}" data-target="#myModalPrev"
+    href="" data-toggle="modal">${item.date} - ${item.type}</a>
+  `);
+  });
+};
+subject.forEach((subName, ind) => {
+  $("#accordionExample").append(
+    `
+              <div class="card" data-toggle="collapse" data-target="#collapse${ind}"
+              aria-expanded="false" aria-controls="collapse${ind}">
+                <div class="card-header" id="heading${ind}">
+                    <h2 class="mb-0">
+                        <div class = "container-fluid text-muted fs-6 " style="cursor:default">
+                            ${subName?.name}
+                        </div>
+                    </h2>
+                </div>
+                <div id="collapse${ind}" class="collapse " aria-labelledby="heading${ind}" data-parent="#accordionExample">
+                    <div class="card-body list-group" id=${subName?.code}>
+                    </div>
+                </div>
+              </div>
+            `
+  );
+  add_Data(subName?.code, subName?.data);
+});
+
+
